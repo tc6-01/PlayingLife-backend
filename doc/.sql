6556aafecf12168d1a74967f33455078d88681ac
BeerAn <2001100003@e.gzhu.edu.cn>
2023-02-08 20:08:03 +0800

    correct config with prodction

---
+++ doc/.sql
@@ -0,0 +1,23 @@
+create table beeran.user
+(
+    id          bigint auto_increment comment 'id
+'
+        primary key,
+    username    varchar(256)                       null comment '用户昵称',
+    _account    varchar(256)                       null comment '账号',
+    avatar_url  varchar(1024)                      null comment '头像地址',
+    gender      tinyint                            null comment '性别',
+    _password   varchar(256)                       not null comment '密码',
+    phone       varchar(256)                       null comment '电话',
+    email       varchar(512)                       null comment '邮箱',
+    create_time datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
+    _status     int                                null comment '是否有效',
+    is_delete   tinyint  default 0                 not null comment '是否删除',
+    role        int      default 0                 not null comment '用户权限
+0 普通用户
+1 管理员',
+    planetCode  varchar(512)                       null comment '星球编号'
+)
+    comment 'table';
+
