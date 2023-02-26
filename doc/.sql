66da5c4dd3fbb008c7a276f82c98844f7ffc66b0
BeerAn <2001100003@e.gzhu.edu.cn>
2023-02-26 10:27:40 +0800

    ADD创建队伍表  添加建表语句进doc文件

--- doc/.sql
+++ doc/.sql
@@ -1,4 +1,4 @@
-create table beeran.user
+create table user
 (
     id          bigint auto_increment comment 'id
 '
@@ -34,3 +34,43 @@ ALTER TABLE user MODIFY phone varchar(256) DEFAULT NULL;
 ALTER TABLE user MODIFY tags varchar(1024) DEFAULT NULL;
 ALTER TABLE user MODIFY profile varchar(1024) DEFAULT NULL;
 show create table user;
+
+-- auto-generated definition
+create table tag
+(
+    id          bigint auto_increment comment 'id'
+        primary key,
+    tagName     varchar(256)                       null comment '标签名称',
+    userID      varchar(256)                       null comment '用户ID',
+    parentID    bigint                             null comment '父标签ID',
+    isParent    tinyint                            null comment '是否为父标签 0为是， 1为否',
+    create_time datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    update_time datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP comment '更新时间',
+    is_delete   tinyint  default 0                 not null comment '是否删除',
+    constraint uinIdx_tag_tagName
+        unique (tagName)
+)
+    comment '标签';
+
+create index idx_userID
+    on tag (userID);
+
+
+create table team
+(
+    id           bigint auto_increment comment 'id'
+        primary key,
+    name   varchar(256)                   not null comment '队伍名称',
+    description varchar(1024)                      null comment '描述',
+    maxNum    int      default 1                 not null comment '最大人数',
+    expireTime    datetime  null comment '过期时间',
+    userId            bigint comment '用户id',
+    status    int      default 0                 not null comment '0 - 公开，1 - 私有，2 - 加密',
+    password varchar(512)                       null comment '密码',
+
+    createTime   datetime default CURRENT_TIMESTAMP null comment '创建时间',
+    updateTime   datetime default CURRENT_TIMESTAMP null on update CURRENT_TIMESTAMP,
+    isDelete     tinyint  default 0                 not null comment '是否删除'
+)
+    comment '队伍';
+
